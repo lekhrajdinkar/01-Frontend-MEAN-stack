99d0285362af842667a31767632e0ae7ad788c6f
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-04-22 19:05:18 -0700

    ignore

--- src/app/common/about.component.ts
+++ src/app/common/about.component.ts
@@ -4,14 +4,16 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 @Component({
     selector: 'app-about',
     template: `
-    <h1> About TACT</h1>
+    <h1> About MEAN STACK tool</h1>
 
 <mat-dialog-content>
 Version : {{passedData.version}}
+<p> Demo application </p>
+<p> Developer : Lekhraj Dinkar </p>
 </mat-dialog-content>
-
+<mat-divider></mat-divider>
 <mat-dialog-actions>
-  <button mat-raised-button color="primary"[mat-dialog-close]="true">Close</button>
+  <button mat-raised-button color="accent"[mat-dialog-close]="true">Close</button>
 </mat-dialog-actions>`
 })
   export class AboutComponent implements OnInit {
